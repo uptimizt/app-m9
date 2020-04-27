1b1a0ac81aebd231c1c7edfc3f9c01f2bac95252
uptimizt <uptimizt@gmail.com>
2020-04-27 16:41:30 +0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){function n(e){return(n="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e})(e)}function r(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}function o(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function c(e,t,n){return t&&o(e.prototype,t),n&&o(e,n),e}function u(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function");e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,writable:!0,configurable:!0}}),t&&a(e,t)}function a(e,t){return(a=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}function l(e){return function(){var t,n=p(e);if(f()){var r=p(this).constructor;t=Reflect.construct(n,arguments,r)}else t=n.apply(this,arguments);return i(this,t)}}function i(e,t){return!t||"object"!==n(t)&&"function"!=typeof t?function(e){if(void 0===e)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return e}(e):t}function f(){if("undefined"==typeof Reflect||!Reflect.construct)return!1;if(Reflect.construct.sham)return!1;if("function"==typeof Proxy)return!0;try{return Date.prototype.toString.call(Reflect.construct(Date,[],(function(){}))),!0}catch(e){return!1}}function p(e){return(p=Object.setPrototypeOf?Object.getPrototypeOf:function(e){return e.__proto__||Object.getPrototypeOf(e)})(e)}var s=function(e){u(n,React.Component);var t=l(n);function n(){return r(this,n),t.apply(this,arguments)}return c(n,[{key:"render",value:function(){var e=this.props.category;return React.createElement("tr",null,React.createElement("th",{colSpan:"2"},e))}}]),n}(),y=function(e){u(n,React.Component);var t=l(n);function n(){return r(this,n),t.apply(this,arguments)}return c(n,[{key:"render",value:function(){var e=this.props.product,t=e.stocked?e.name:React.createElement("span",{style:{color:"red"}},e.name);return React.createElement("tr",null,React.createElement("td",null,t),React.createElement("td",null,e.price))}}]),n}(),m=function(e){u(n,React.Component);var t=l(n);function n(){return r(this,n),t.apply(this,arguments)}return c(n,[{key:"render",value:function(){var e=[],t=null;return this.props.products.forEach((function(n){n.category!==t&&e.push(React.createElement(s,{category:n.category,key:n.category})),e.push(React.createElement(y,{product:n,key:n.name})),t=n.category})),React.createElement("table",null,React.createElement("thead",null,React.createElement("tr",null,React.createElement("th",null,"Name"),React.createElement("th",null,"Price"))),React.createElement("tbody",null,e))}}]),n}(),d=function(e){u(n,React.Component);var t=l(n);function n(){return r(this,n),t.apply(this,arguments)}return c(n,[{key:"render",value:function(){return React.createElement("form",null,React.createElement("input",{type:"text",placeholder:"Search..."}),React.createElement("p",null,React.createElement("input",{type:"checkbox"})," ","Only show products in stock"))}}]),n}(),b=function(e){u(n,React.Component);var t=l(n);function n(){return r(this,n),t.apply(this,arguments)}return c(n,[{key:"render",value:function(){return React.createElement("div",null,React.createElement(d,null),React.createElement(m,{products:this.props.products}))}}]),n}();ReactDOM.render(React.createElement(b,{products:[{category:"Sporting Goods",price:"$49.99",stocked:!0,name:"Football 2"},{category:"Sporting Goods",price:"$9.99",stocked:!0,name:"Baseball"},{category:"Sporting Goods",price:"$29.99",stocked:!1,name:"Basketball"},{category:"Electronics",price:"$99.99",stocked:!0,name:"iPod Touch"},{category:"Electronics",price:"$399.99",stocked:!1,name:"iPhone 5"},{category:"Electronics",price:"$199.99",stocked:!0,name:"Nexus 7"}]}),document.getElementById("app-fitable"))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function _typeof(obj) { \"@babel/helpers - typeof\"; if (typeof Symbol === \"function\" && typeof Symbol.iterator === \"symbol\") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === \"function\" && obj.constructor === Symbol && obj !== Symbol.prototype ? \"symbol\" : typeof obj; }; } return _typeof(obj); }\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nfunction _inherits(subClass, superClass) { if (typeof superClass !== \"function\" && superClass !== null) { throw new TypeError(\"Super expression must either be null or a function\"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }\n\nfunction _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }\n\nfunction _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }\n\nfunction _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === \"object\" || typeof call === \"function\")) { return call; } return _assertThisInitialized(self); }\n\nfunction _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError(\"this hasn't been initialised - super() hasn't been called\"); } return self; }\n\nfunction _isNativeReflectConstruct() { if (typeof Reflect === \"undefined\" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === \"function\") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }\n\nfunction _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }\n\nvar ProductCategoryRow = /*#__PURE__*/function (_React$Component) {\n  _inherits(ProductCategoryRow, _React$Component);\n\n  var _super = _createSuper(ProductCategoryRow);\n\n  function ProductCategoryRow() {\n    _classCallCheck(this, ProductCategoryRow);\n\n    return _super.apply(this, arguments);\n  }\n\n  _createClass(ProductCategoryRow, [{\n    key: \"render\",\n    value: function render() {\n      var category = this.props.category;\n      return /*#__PURE__*/React.createElement(\"tr\", null, /*#__PURE__*/React.createElement(\"th\", {\n        colSpan: \"2\"\n      }, category));\n    }\n  }]);\n\n  return ProductCategoryRow;\n}(React.Component);\n\nvar ProductRow = /*#__PURE__*/function (_React$Component2) {\n  _inherits(ProductRow, _React$Component2);\n\n  var _super2 = _createSuper(ProductRow);\n\n  function ProductRow() {\n    _classCallCheck(this, ProductRow);\n\n    return _super2.apply(this, arguments);\n  }\n\n  _createClass(ProductRow, [{\n    key: \"render\",\n    value: function render() {\n      var product = this.props.product;\n      var name = product.stocked ? product.name : /*#__PURE__*/React.createElement(\"span\", {\n        style: {\n          color: 'red'\n        }\n      }, product.name);\n      return /*#__PURE__*/React.createElement(\"tr\", null, /*#__PURE__*/React.createElement(\"td\", null, name), /*#__PURE__*/React.createElement(\"td\", null, product.price));\n    }\n  }]);\n\n  return ProductRow;\n}(React.Component);\n\nvar ProductTable = /*#__PURE__*/function (_React$Component3) {\n  _inherits(ProductTable, _React$Component3);\n\n  var _super3 = _createSuper(ProductTable);\n\n  function ProductTable() {\n    _classCallCheck(this, ProductTable);\n\n    return _super3.apply(this, arguments);\n  }\n\n  _createClass(ProductTable, [{\n    key: \"render\",\n    value: function render() {\n      var rows = [];\n      var lastCategory = null;\n      this.props.products.forEach(function (product) {\n        if (product.category !== lastCategory) {\n          rows.push( /*#__PURE__*/React.createElement(ProductCategoryRow, {\n            category: product.category,\n            key: product.category\n          }));\n        }\n\n        rows.push( /*#__PURE__*/React.createElement(ProductRow, {\n          product: product,\n          key: product.name\n        }));\n        lastCategory = product.category;\n      });\n      return /*#__PURE__*/React.createElement(\"table\", null, /*#__PURE__*/React.createElement(\"thead\", null, /*#__PURE__*/React.createElement(\"tr\", null, /*#__PURE__*/React.createElement(\"th\", null, \"Name\"), /*#__PURE__*/React.createElement(\"th\", null, \"Price\"))), /*#__PURE__*/React.createElement(\"tbody\", null, rows));\n    }\n  }]);\n\n  return ProductTable;\n}(React.Component);\n\nvar SearchBar = /*#__PURE__*/function (_React$Component4) {\n  _inherits(SearchBar, _React$Component4);\n\n  var _super4 = _createSuper(SearchBar);\n\n  function SearchBar() {\n    _classCallCheck(this, SearchBar);\n\n    return _super4.apply(this, arguments);\n  }\n\n  _createClass(SearchBar, [{\n    key: \"render\",\n    value: function render() {\n      return /*#__PURE__*/React.createElement(\"form\", null, /*#__PURE__*/React.createElement(\"input\", {\n        type: \"text\",\n        placeholder: \"Search...\"\n      }), /*#__PURE__*/React.createElement(\"p\", null, /*#__PURE__*/React.createElement(\"input\", {\n        type: \"checkbox\"\n      }), ' ', \"Only show products in stock\"));\n    }\n  }]);\n\n  return SearchBar;\n}(React.Component);\n\nvar FilterableProductTable = /*#__PURE__*/function (_React$Component5) {\n  _inherits(FilterableProductTable, _React$Component5);\n\n  var _super5 = _createSuper(FilterableProductTable);\n\n  function FilterableProductTable() {\n    _classCallCheck(this, FilterableProductTable);\n\n    return _super5.apply(this, arguments);\n  }\n\n  _createClass(FilterableProductTable, [{\n    key: \"render\",\n    value: function render() {\n      return /*#__PURE__*/React.createElement(\"div\", null, /*#__PURE__*/React.createElement(SearchBar, null), /*#__PURE__*/React.createElement(ProductTable, {\n        products: this.props.products\n      }));\n    }\n  }]);\n\n  return FilterableProductTable;\n}(React.Component);\n\nvar PRODUCTS = [{\n  category: 'Sporting Goods',\n  price: '$49.99',\n  stocked: true,\n  name: 'Football 2'\n}, {\n  category: 'Sporting Goods',\n  price: '$9.99',\n  stocked: true,\n  name: 'Baseball'\n}, {\n  category: 'Sporting Goods',\n  price: '$29.99',\n  stocked: false,\n  name: 'Basketball'\n}, {\n  category: 'Electronics',\n  price: '$99.99',\n  stocked: true,\n  name: 'iPod Touch'\n}, {\n  category: 'Electronics',\n  price: '$399.99',\n  stocked: false,\n  name: 'iPhone 5'\n}, {\n  category: 'Electronics',\n  price: '$199.99',\n  stocked: true,\n  name: 'Nexus 7'\n}];\nReactDOM.render( /*#__PURE__*/React.createElement(FilterableProductTable, {\n  products: PRODUCTS\n}), document.getElementById('app-fitable'));\n\n//# sourceURL=webpack:///./src/app.js?");
+
+/***/ })
+
+/******/ });
